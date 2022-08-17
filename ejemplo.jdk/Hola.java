c6527ac95f0e7461adbb5417f3ad9e44b7023387
Marco <marcosolano234@gmail.com>
2022-08-17 01:26:36 -0500

    ultimo cambio

---
+++ ejemplo.jdk/Hola.java
@@ -0,0 +1,17 @@
+public class Hola {
+//esto es una clase del jdk de java
+
+
+
+        public static void main(String[] arg){
+        //es la declaración del método main
+
+                //String miVariable = "Hola Mundo Java sañudo a:"
+                String miNombre = "Marco Solano";
+
+                System.out.println(" Hola saludos a: " + miNombre);
+
+        }
+
+}
+
